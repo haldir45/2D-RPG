5e92d8c3d4a16d6c8c07bfa9927bd416d2394f0e
Tsantas Haris <ctsantas@arx.gr>
2019-04-15 18:51:20 +0300

    added initial movement for player

---
+++ 2D-RPG/Assets/Scripts/Character.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    /// <summary>
+    /// The Player's direction
+    /// </summary>
+    protected Vector2 direction;
+
+    /// <summary>
+    /// The Player's movement speed
+    /// </summary>
+    [SerializeField]
+    private float speed;
+
+    /// <summary>
+    /// They Player's Animator
+    /// </summary>
+    private Animator animator;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+       animator = GetComponent<Animator>();
+      
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+        AnimateMovement(direction);
+    }
+
+    public void AnimateMovement(Vector2 direction)
+    {
+        animator.SetFloat("x", direction.x);
+        animator.SetFloat("y", direction.y);
+    }
+
+}
