643634af54a9f1947559745ab50b4b63c2d72c96
Tsantas Haris <ctsantas@arx.gr>
2019-04-23 18:10:10 +0300

    added ui manager controller

---
+++ 2D-RPG/Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GameManager : MonoBehaviour
+{
+    /// <summary>
+    /// The GameManager's player's reference
+    /// </summary>
+    [SerializeField]
+    private Player player;
+
+    /// <summary>
+    /// Clickable layer
+    /// </summary>
+    private const int clickableLayerMask = 1 << 9;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ClickTarget();
+    }
+
+    /// <summary>
+    /// Sets player's target to mouse's position when player clicks in the world
+    /// </summary>
+    private void ClickTarget()
+    {
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity, clickableLayerMask);
+            
+            if(hit.collider != null)
+            {
+                if(hit.collider.tag == "Enemy")
+                player.Target = hit.transform;
+            }
+            else
+            {
+                player.Target = null;
+            }
+        }
+     
+    }
+}
---
+++ 2D-RPG/Assets/Scripts/UIManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    /// <summary>
+    /// The action buttons in the action bar
+    /// </summary>
+    [SerializeField]
+    private Button[] actionButtons;
+
+    /// <summary>
+    /// The keycodes of the action buttons
+    /// </summary>
+    private KeyCode action1, action2, action3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        action1 = KeyCode.Alpha1;
+        action2 = KeyCode.Alpha2;
+        action3 = KeyCode.Alpha3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(action1))
+        {
+            ActionButtonOnClick(0);
+        }
+
+        if (Input.GetKeyDown(action2))
+        {
+            ActionButtonOnClick(1);
+
+        }
+
+        if (Input.GetKeyDown(action3))
+        {
+            ActionButtonOnClick(2);
+
+        }
+    }
+
+    /// <summary>
+    /// Triggers the action button's onClick event
+    /// </summary>
+    /// <param name="buttonIndex"></param>
+    private void ActionButtonOnClick(int buttonIndex)
+    {
+        actionButtons[buttonIndex].onClick.Invoke();
+    }
+}
